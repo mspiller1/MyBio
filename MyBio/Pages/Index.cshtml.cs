e2557cd25fbb89a007fc33d8d1d54821a6cc9c4f
mspiller <jorelspiller@gmail.com>
2023-03-27 13:16:26 -0400

    Rename FirstAzureApp to MyBio across the solution.

--- MyBio/Pages/Index.cshtml.cs
+++ MyBio/Pages/Index.cshtml.cs
@@ -1,7 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace FirstAzureApp.Pages
+namespace MyBio.Pages
 {
     public class IndexModel : PageModel
     {
